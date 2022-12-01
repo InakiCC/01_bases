8bd8f720f35ff403866cf34451cda0a01ace2c39
Inaki Cuadra <inaki.cuadra@gmail.com>
2022-12-01 10:07:09 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  // template: 
+  //   <h1>hOLA DE NUEVO</h1>
+  //   Esto es textod eprueba<br>
+  //   Texto de prueba2  
+  // 
+  templateUrl: './app.component.html',
+  //styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+}
